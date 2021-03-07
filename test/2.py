f057114a2ab16e4eb85f836c65adcafa7bddc03b
aimho23 <aimho23@gmail.com>
2021-03-07 10:06:47 +0900

    문제2

---
+++ test/2.py
@@ -0,0 +1,15 @@
+#카지노
+
+a, b = list(map(int, input().split()))
+data = list(map(int, input().split()))
+
+result = 0
+lenth = len(data)
+
+for i in range(0, lenth) :
+    for j in range(i+1, lenth):
+        for k in range(j+1, lenth) :
+            sum_value = data[i] +data[j] + data[k]
+                if sum_value <= b:
+                    result = max(result, sum_value)
+print(result)
