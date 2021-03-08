016ececf52b1e92b15847003a3c97f751a1f4de1
aimho23 <aimho23@gmail.com>
2021-03-08 23:24:03 +0900

    문제3 스택

--- test/2.py
+++ test/2.py
@@ -11,6 +11,7 @@ for i in range(0, lenth) :
     for j in range(i+1, lenth):
         for k in range(j+1, lenth) :
             sum_value = data[i] +data[j] + data[k]
-                if sum_value <= b:
+            if sum_value <= b:
                     result = max(result, sum_value)
+
 print(result)
